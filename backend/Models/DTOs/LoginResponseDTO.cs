b254fbe68fb2c5e92b645e9c7eeb7af897cdd798
Paolagarcia02 <pgpzaragoza@gmail.com>
2026-02-26 20:38:51 +0100

    Añadido backend

---
+++ backend/Models/DTOs/LoginResponseDTO.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SaviaVetAPI.DTOs;
+
+    public class LoginResponseDTO
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
---
+++ backend/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,17 @@
+using System.Security.Claims;
+using SaviaVetAPI.DTOs; 
+
+
+namespace SaviaVetAPI.Services
+{
+    public interface IAuthService
+    {
+        Task<LoginResponseDTO?> Login(LoginDtoIn loginDtoIn);
+        
+        Task<string> Register(UserDtoIn userDtoIn);
+        
+        string GenerateToken(UserDtoOut userDtoOut);
+        
+        bool HasAccessToResource(int requestedUserID, ClaimsPrincipal user);
+    }
+}
