9029c3fc9d9f16ae820bf01eee30957acc082f39
Paolagarcia02 <pgpzaragoza@gmail.com>
2026-03-01 01:53:53 +0100

    general config

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import { RouterView } from 'vue-router'
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+import LoginView from './views/LoginView.vue'
+
+
+</script>
+
+<template>
+
+  <div>
+    <router-view />
+  </div>
+
+</template>
+
+<style scoped>
+@import './assets/styles/main.scss';
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+main {
+  flex: 1;
+}
+</style>
